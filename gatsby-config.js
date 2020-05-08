f34b055f1776ec7bcead5170789d5291d2ee1c0d
Baye <baye@wayly.net>
2020-05-08 23:00:09 +0800

    update site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ServerCat`,
+    description: `Server status, docker management and ssh terminal `,
+    author: `@waylybaye`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>ServerCat</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>ServerCat is cool</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/*<Link to="/page-2/">Go to page 2</Link>*/}
   </Layout>
 )
 
